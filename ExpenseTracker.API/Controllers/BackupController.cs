eda7d5473d66816a858fe1ad40322b8a874946ec
Alp <alpbalaj1203@gmail.com>
2026-01-27 17:03:19 +0100

    Trying to make backup, export available, and then import to have backups of user data.

---
+++ ExpenseTracker.API/Controllers/BackupController.cs
@@ -0,0 +1,27 @@
+﻿using ExpenseTracker.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public sealed class BackupController : ControllerBase
+{
+    private readonly IBackupExportService _exportService;
+
+    public BackupController(IBackupExportService exportService)
+    {
+        _exportService = exportService;
+    }
+
+    [HttpGet("export")]
+    public async Task Export(CancellationToken ct)
+    {
+        Response.ContentType = "application/zip";
+        var fileName = $"backup_{DateTime.UtcNow:yyyy-MM-dd}.zip";
+        Response.Headers.ContentDisposition = $"attachment; filename=\"{fileName}\"";
+
+        // Important: no userId param. Always use the authenticated user.
+        await _exportService.WriteExportZipAsync(Response.Body, ct);
+    }
+}
---
+++ ExpenseTracker.Application/Interfaces/IBackupExportService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Application.Interfaces
+{
+    public interface IBackupExportService
+    {
+        Task WriteExportZipAsync(Stream output, CancellationToken cancellationToken);
+    }
+}
